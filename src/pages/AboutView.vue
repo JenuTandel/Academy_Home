8ec226768985f84d8485fa2906e138d4be1a24ac
Jinal Tandel <jinal.tandel@1rivet.local>
2023-04-24 17:13:04 +0530

    about-us contact-us UI

--- src/pages/AboutView.vue
+++ src/pages/AboutView.vue
@@ -1,2 +1,62 @@
-<template><h1>About View</h1></template>
+<template>
+  <div class="d-flex flex-column h-100">
+    <div class="image-wrapper">
+      <img src="../assets/images/about.jpg" />
+    </div>
+    <div
+      class="text-center mt-3 d-flex flex-column flex-grow-1 justify-content-center"
+    >
+      <h3>Our Story</h3>
+      <p>
+        We provide easy learning with your interested area and helps you to
+        achieve your task.
+      </p>
+    </div>
+    <!-- start: card-wrapper -->
+    <div class="bg-primary w-100 d-flex justify-content-center">
+      <div class="card-wrapper d-flex justify-content-between text-center">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title mb-4">What are we?</h5>
+            <p class="card-text">
+              We provide easy learning with your interested area and helps you
+              to achieve your task.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title mb-4">What we do?</h5>
+            <p class="card-text">
+              We provide easy learning with your interested area and helps you
+              to achieve your task.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end: card-wrapper -->
+  </div>
+</template>
 <script lang="ts"></script>
+<style scoped lang="scss">
+.image-wrapper {
+  height: 400px;
+  width: 100%;
+
+  img {
+    height: 100%;
+    width: 100%;
+  }
+}
+.card-wrapper {
+  width: 600px;
+  margin-top: -50px;
+  margin-bottom: 30px;
+
+  .card {
+    height: 230px;
+    width: 230px;
+  }
+}
+</style>
--- src/pages/ContactView.vue
+++ src/pages/ContactView.vue
@@ -1,2 +1,87 @@
-<template><h1>Contact View</h1></template>
+<template>
+  <div class="d-flex flex-column h-100">
+    <div class="image-wrapper">
+      <img src="../assets/images/contact.jpg" />
+    </div>
+    <!-- start: contact form -->
+    <div
+      class="d-flex justify-content-center align-items-center flex-column flex-grow-1"
+    >
+      <h3 class="text-center text-primary">We want to here you</h3>
+      <form class="border rounded p-3 mt-3">
+        <div class="mb-3">
+          <input
+            type="text"
+            id="name"
+            class="form-control"
+            placeholder="Name"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="text"
+            id="email"
+            class="form-control"
+            placeholder="Email"
+          />
+        </div>
+        <div class="mb-3">
+          <textarea
+            class="form-control"
+            id="message"
+            rows="3"
+            placeholder="Your Query"
+          ></textarea>
+        </div>
+        <button type="button" class="form-control bg-secondary text-white">
+          Submit
+        </button>
+      </form>
+    </div>
+    <!-- end: contact form-->
+    <!-- start: contact footer -->
+    <div
+      class="d-flex justify-content-between bg-primary align-items-center text-white py-3 px-3"
+    >
+      <div class="text-center">
+        <p>Call Us</p>
+        <p><span class="icon-phone me-2"></span>9033982833</p>
+      </div>
+      <div class="text-center">
+        <p>Email Us</p>
+        <a href="mailto:jinaltandel06@gmail.com"
+          ><span class="icon-envelop me-2"></span
+          ><span class="text-white">jinaltandel06@gmail.com</span></a
+        >
+      </div>
+      <div class="text-center">
+        <p>Contact with Us</p>
+        <p>
+          <span class="icon-facebook me-2"></span>
+          <span class="icon-instagram me-2"></span>
+          <span class="icon-twitter me-2"></span>
+          <span class="icon-envelop"></span>
+        </p>
+      </div>
+    </div>
+    <!-- end: contact footer -->
+  </div>
+</template>
 <script lang="ts"></script>
+
+<style scoped lang="scss">
+.image-wrapper {
+  width: 100%;
+  height: 400px;
+  img {
+    height: 100%;
+    width: 100%;
+  }
+}
+form {
+  width: 400px;
+  a {
+    cursor: pointer;
+  }
+}
+</style>
