005dc09abb7c6280df70633766fb0bf36f5f9ede
Jinal Tandel <jinal.tandel@1rivet.local>
2023-04-24 17:39:51 +0530

    homepage UI

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,2 +1,24 @@
-<template><h1>Home View</h1></template>
+<template>
+  <div class="row gx-0 h-100 align-items-center">
+    <div class="col-7 d-flex justify-content-center">
+      <img src="../assets/images/home_bg.png" class="shadow" />
+    </div>
+    <div class="col-5 d-flex flex-column align-items-center p-3">
+      <h1 class="text-center text-primary mb-4">
+        <span class="d-block"> Explore your creativity </span>
+        <span> with your personal guide </span>
+      </h1>
+      <p class="text-primary fs-4 mb-4 text-center">
+        Get experienced expert in specialized area to get knowledge and
+        instruction for your work
+      </p>
+      <button
+        type="button"
+        class="btn btn-secondary text-white fw-semibold px-5 py-2"
+      >
+        <span class="fs-5"> Get Started </span>
+      </button>
+    </div>
+  </div>
+</template>
 <script lang="ts"></script>
